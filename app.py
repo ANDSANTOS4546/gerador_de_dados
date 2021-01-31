d09dc9b8d865993f0e9edef201caf55e3c2d7561
André Nascimento dos Santos <andre_nascimento4546@hotmail.com>
2021-01-31 14:48:19 -0300

    Atualização de variaveis

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ while True:
         'Gerar novos dados',
         'Sair do sistema'
     ])
-    sel = opcao_menu_principal()
+    selecao = opcao_menu_principal()
 
     # Opção para exibir os dados gerados
-    if sel == 1:
+    if selecao == 1:
         system('cls')
         cabecalho('DADOS GERADOS')
         lerArquivo(dados)
@@ -27,7 +27,7 @@ while True:
         input('Enter para continuar...')
 
     # Opção para gerar novos dados
-    elif sel == 2:
+    elif selecao == 2:
         system('cls')
         cabecalho('GERAR NOVOS DADOS')
         print('Escolha uma ou mais opções abaixo:')
@@ -38,18 +38,18 @@ while True:
             'Cidade',
             'Estado',
         ])
-        op = opcaoCadastro()
+        opcao = opcaoCadastro()
         print('Gerando dados...')
 
         # Gerar dados da(s) opção(ões) selecionada(s)
-        for item in op:
+        for item in opcao:
             sorteio = sortearDados(item)
             gerar(dados, sorteio)
         sleep(1)
         print('Dados gerados com sucesso.')
 
     # Opção para encerrar o sistema
-    elif sel == 3:
+    elif selecao == 3:
         system('cls')
         cabecalho('Saindo... Até a próxima.')
         sleep(1)
